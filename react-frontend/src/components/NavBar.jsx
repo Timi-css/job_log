6fec66ad9e8cc355e1759018051854bb4554b4bd
Timi <alabitimi65@gmail.com>
2023-01-11 17:10:21 -0700

    create react compnonets - client side setup

---
+++ react-frontend/src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../styles/Navbar.css";
+
+const NavBar = () => {
+  return (
+    <div className="main-nav-container">
+      <div className="logo-box">
+        <a href="/" className="logo">
+          JOBLOG
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
